dad1d7c02f831e65b824fed754bc384a471a9539
danielcieslinski <daniel98c@gmail.com>
2019-05-25 19:30:14 +0200

    lets try

---
+++ deamon.py
@@ -0,0 +1,43 @@
+import RPi.GPIO as GPIO
+from terminator_api import *
+from constants import *
+
+CANDLE_DISTANCE = 15
+
+
+class Daemon:
+    def __init__(self):
+        self.vehicle = Vehicle((RIGHT_FORWARD, RIGHT_BACKWARD), (LEFT_FORWARD, LEFT_BACKWARD))
+        self.fan = Controller(FAN_FORWARD, FAN_BACKWARD)
+        self.hc_sensor = HCSensor(ECHO_PIN, TRIGGER_PIN)
+        self.flame_sensor = Sensor(FLAME_PIN)
+
+    def loop(self):
+        while True:
+            self.find_flame()
+            if self.drive_while_can(): self.extinguish()
+            else: pass
+
+    def find_flame(self):
+        while True:
+            self.vehicle.turn_right(0.01)
+            if self.flame_sensor.check():
+                return
+
+    def drive_while_can(self):
+        while self.hc_sensor.distance() > CANDLE_DISTANCE and self.flame_sensor.check():
+            self.vehicle.forward(0.02)
+
+        return self.flame_sensor.check()
+
+    def extinguish(self):
+        self.fan.forward(5)
+        self.vehicle.backward(1)
+
+
+if __name__ == '__main__':
+    daemon = Daemon()
+    try:
+        daemon.loop()
+    except:
+        GPIO.cleanup()
--- terminator_api.py
+++ terminator_api.py
@@ -1,10 +1,8 @@
 import RPi.GPIO as GPIO
 from time import sleep, time
 import threading
-from constants import *
 
-
-class WheelsLine:
+class Controller:
     def __init__(self, forward_pin, backward_pin):
         self.forward_pin = forward_pin
         self.backward_pin = backward_pin
@@ -22,132 +20,89 @@ class WheelsLine:
         sleep(duration)
         GPIO.output(self.backward_pin, GPIO.LOW)
 
-    def __del__(self):
-        GPIO.cleanup()
 
 class Vehicle:
     def __init__(self, right_pins, left_pins):
-        self.right_wheels = WheelsLine(right_pins[0], right_pins[1])
-        self.left_wheels = WheelsLine(left_pins[0], left_pins[1])
+        self.right_wheels = Controller(right_pins[0], right_pins[1])
+        self.left_wheels = Controller(left_pins[0], left_pins[1])
 
     def forward(self, duration):
-        ths = [threading.Thread(target=line.forward, args=(duration,)) for line in [self.left_wheels, self.right_wheels] ]
-        for th in ths: th.start()
-        for th in ths: th.join()
+        return self.__run(duration, [self.left_wheels, self.right_wheels])
 
     def backward(self, duration):
-        ths = [threading.Thread(target=line.backward, args=(duration,)) for line in [self.left_wheels, self.right_wheels] ]
-        for th in ths: th.start()
-        for th in ths: th.join()
+        return self.__run(duration, [self.left_wheels, self.right_wheels])
 
     def turn_left(self, duration):
-        ths = [threading.Thread(target=line, args=(duration,)) for line in [ self.left_wheels.backward, self.right_wheels.forward ] ]
-        for th in ths: th.start()
-        for th in ths: th.join()
+        return self.__run(duration, [self.left_wheels.backward, self.right_wheels.forward])
 
     def turn_right(self, duration):
-        ths = [threading.Thread(target=line, args=(duration,)) for line in [ self.left_wheels.forward, self.right_wheels.backward ] ]
+        return self.__run(duration, [self.left_wheels.forward, self.right_wheels.backward])
+
+    def __run(self, duration, args):
+        ths = [threading.Thread(target=line, args=(duration,)) for line in args]
         for th in ths: th.start()
         for th in ths: th.join()
 
-    def __del__(self):
-        del self.right_wheels, self.left_wheels
 
 class Sensor:
-    def __init__(self, pin, sensor_type):
+    def __init__(self, pin):
         self.pin = pin
-        self.sensor_type = sensor_type
-        GPIO.setmode(GPIO.BOARD)  # Set GPIO by numbers
+        GPIO.setmode(GPIO.BOARD)
         GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     def check(self):
         return (GPIO.input(self.pin) == 0)
 
     def loop(self):
+        """
+        Only for debug. Use check in deamon
+        """
         while True:
             if (self.check()):
-                print("{}: Detected".format(self.sensor_type))
-            else: print("{}: ----".format(self.sensor_type))
+                print("Detected")
+            else:
+                print("----")
 
-    def __del__(self):
-        GPIO.cleanup()
 
 class HCSensor:
-    def __init__(self):
+    def __init__(self, trigger_pin, echo_pin):
+        self.trigger_pin = trigger_pin
+        self.echo_pin = echo_pin
 
         GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(TRIGGER_PIN, GPIO.OUT)
-        GPIO.setup(ECHO_PIN, GPIO.IN)
+        GPIO.setup(self.trigger_pin, GPIO.OUT)
+        GPIO.setup(self.echo_pin, GPIO.IN)
 
     def distance(self):
-        GPIO.output(TRIGGER_PIN, True)
-        # set Trigger after 0.01ms to LOW
+        GPIO.output(self.trigger_pin, True)
         sleep(0.00001)
-        GPIO.output(TRIGGER_PIN, False)
+        GPIO.output(self.trigger_pin, False)
 
-        StartTime = time()
-        StopTime = time()
+        start_time = time()
+        stop_time = time()
 
-        while GPIO.input(ECHO_PIN) == 0:
-            StartTime = time()
+        while GPIO.input(self.echo_pin) == 0:
+            start_time = time()
 
-        while GPIO.input(ECHO_PIN) == 1:
-            StopTime = time()
+        while GPIO.input(self.echo_pin) == 1:
+            start_time = time()
 
-        TimeElapsed = StopTime - StartTime
-        distance = (TimeElapsed * 34300) / 2
+        time_elapsed = stop_time - start_time
+        distance = (time_elapsed * 34300) / 2
 
         return distance
 
     def loop(self, interval):
+        """
+        Only for debug, use distance in daemon
+        """
         try:
             while True:
                 dist = self.distance()
                 print("Measured Distance = %.1f cm" % dist)
                 sleep(interval)
 
-            # Reset by pressing CTRL + C
         except KeyboardInterrupt:
             print("Measurement stopped by User")
             GPIO.cleanup()
 
-class Fan:
-    def __init__(self, forward_pin, backward_pin):
-        self.forward_pin = forward_pin
-        self.backward_pin = backward_pin
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(self.forward_pin, GPIO.OUT)
-        GPIO.setup(self.backward_pin, GPIO.OUT)
-
-    def forward(self, duration):
-        GPIO.output(self.forward_pin, GPIO.HIGH)
-        sleep(duration)
-        GPIO.output(self.forward_pin, GPIO.LOW)
-
-    def backward(self, duration):
-        GPIO.output(self.backward_pin, GPIO.HIGH)
-        sleep(duration)
-        GPIO.output(self.backward_pin, GPIO.LOW)
-
-    def __del__(self):
-        GPIO.cleanup()
-
-
-if __name__ == '__main__':
-
-    # Sensor(32, )
-    # sens = HCSensor()
-    # sens.loop(0.5)
-
-    # fan = Fan(FAN_FORWARD, FAN_BACKWARD)
-    # fan.forward(3)
-
-    sensor = Sensor(FLAME_PIN, "Flame")
-    #
-    # try: oas.loop()
-    # except: GPIO.cleanup()
-
-    # vehicle = Vehicle([16,18], [40,38])
-    # vehicle.forward(5)
-
-    GPIO.cleanup()
